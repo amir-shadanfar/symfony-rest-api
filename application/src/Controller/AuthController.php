43636a550b8ddfa9432e3040203060ba87be6311
Amir <a.shadanfar.IT@gmail.com>
2020-05-16 13:56:02 +0300

    create auth controller & install packages

---
+++ application/src/Controller/AuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class AuthController extends ApiController
+{
+    /**
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $encoder
+     * @return JsonResponse
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $encoder)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $request = $this->transformJsonBody($request);
+        $username = $request->get('username');
+        $password = $request->get('password');
+        $name = $request->get('name');
+
+        if (empty($username) || empty($password) || empty($name)){
+            return $this->respondValidationError("Invalid Username or Password or Name");
+        }
+
+        $user = new User($username);
+        $user->setPassword($encoder->encodePassword($user, $password));
+        $user->setName($name);
+        $em->persist($user);
+        $em->flush();
+        return $this->respondWithSuccess(sprintf('User %s successfully created', $user->getUsername()));
+    }
+
+    /**
+     * @param UserInterface $user
+     * @param JWTTokenManagerInterface $JWTManager
+     * @return JsonResponse
+     */
+    public function getTokenUser(UserInterface $user, JWTTokenManagerInterface $JWTManager)
+    {
+        return new JsonResponse(['token' => $JWTManager->create($user)]);
+    }
+}
